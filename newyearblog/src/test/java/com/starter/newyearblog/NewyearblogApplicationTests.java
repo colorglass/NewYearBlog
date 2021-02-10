5fd773239152f84e91276c14b13e7a63dbc6731c
colorglass <277267828@qq.com>
2021-02-10 16:26:48 +0800

    项目构建

---
+++ newyearblog/src/test/java/com/starter/newyearblog/NewyearblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.newyearblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewyearblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
