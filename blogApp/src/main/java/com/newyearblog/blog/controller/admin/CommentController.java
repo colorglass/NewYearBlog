353121fdc4cec7c8f7eb90c347cbec23deea4500
LosLiSang <52745915+LosLiSang@users.noreply.github.com>
2021-02-28 15:49:34 +0800

    完成了留言的基本管理功能

---
+++ blogApp/src/main/java/com/newyearblog/blog/controller/admin/CommentController.java
@@ -0,0 +1,10 @@
+package com.newyearblog.blog.controller.admin;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/comment")
+public class CommentController {
+}
---
+++ blogApp/src/main/java/com/newyearblog/blog/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.newyearblog.blog.service;
+
+import com.newyearblog.blog.entity.Comment;
+import org.springframework.data.domain.Page;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CommentService {
+
+
+
+    int addComment(Comment comment);
+
+    Page<Comment> queryCommentsPage(int page);
+
+    Page<Comment> queryCommentsByDate(int year, int month, int page);
+
+    List<Integer> findAllYear();
+
+    //删除评论
+    int deleteComment(Long id);
+
+    int getCommentCount();
+}
