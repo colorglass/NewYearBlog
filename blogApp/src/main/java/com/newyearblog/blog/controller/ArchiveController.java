33de592fa87ae5083eec4c21ff3093df6e257695
colorglass <277267828@qq.com>
2021-03-02 21:51:03 +0800

    最后的“波纹”（指博客 0.1完成）

--- blogApp/src/main/java/com/newyearblog/blog/controller/ArchiveController.java
+++ blogApp/src/main/java/com/newyearblog/blog/controller/ArchiveController.java
@@ -1,8 +1,12 @@
 package com.newyearblog.blog.controller;
 
 import com.newyearblog.blog.service.ArchiveService;
+import com.newyearblog.blog.service.TagService;
+import com.newyearblog.blog.service.TypeService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,27 +16,37 @@ import java.util.Map;
 public class ArchiveController {
     @Autowired
     public ArchiveService archiveService;
+    @Autowired
+    private TypeService typeService;
+    @Autowired
+    private TagService tagService;
 
     @RequestMapping("/archive")
-    public String toArchive(Map<String, Object> map){
+    public String toArchive(Map<String, Object> map, Model model) {
         handleYear(map);
         int year = archiveService.getAllYear().get(0);
         handleBlog(map, year);
+        //搜索框用的
+        model.addAttribute("types", typeService.getAllTypes());
+        model.addAttribute("tags", tagService.getAllTags());
         return "archive";
     }
 
     @RequestMapping("archive/{year}")
-    public String archiveByYear(@PathVariable("year") int year, Map<String, Object> map){
+    public String archiveByYear(@PathVariable("year") int year, Map<String, Object> map,Model model) {
         handleYear(map);
         handleBlog(map, year);
+        //搜索框用的
+        model.addAttribute("types", typeService.getAllTypes());
+        model.addAttribute("tags", tagService.getAllTags());
         return "archive";
     }
 
-    private void handleYear(Map<String, Object> map){
+    private void handleYear(Map<String, Object> map) {
         map.put("years", archiveService.getAllYear());
     }
 
-    private void handleBlog(Map<String, Object> map, int year){
+    private void handleBlog(Map<String, Object> map, int year) {
         map.put("currentYear", year);
         map.put("blogOne", archiveService.getBlogByYearAndMonth(year, 1));
         map.put("blogTwo", archiveService.getBlogByYearAndMonth(year, 2));
